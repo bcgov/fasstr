bb257a6a9b7166abf6a0f20edbe507e66af4fc3b
Jon Goetz <JGOETZ@GOVBC>
2017-10-30 17:43:50 -0700

    test

--- R/annual.stats.R
+++ R/annual.stats.R
@@ -333,7 +333,7 @@ annual.stats <- function(station.name=NULL,
   Q.outside.quant <- dplyr::rename(Q.outside.quant,Year=AnalysisYear)
   
   
-  # Combine all and label columnss
+  # Combine all and label columns
   Q.stat <- merge(Q.stat.annual,Q.stat.seasons4,by="Year",all = TRUE)
   Q.stat <- merge(Q.stat,Q.stat.seasons2,by="Year",all = TRUE)
   Q.stat <- merge(Q.stat,Q.stat.month,by="Year",all = TRUE)
