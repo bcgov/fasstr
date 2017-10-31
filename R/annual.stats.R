16fb3ad230de662b5c2ab8759be812af205df88f
jongoetz <jon.goetz@gov.bc.ca>
2017-10-30 19:49:43 -0700

    updates

--- R/annual.stats.R
+++ R/annual.stats.R
@@ -82,20 +82,6 @@
 #'
 #--------------------------------------------------------------
 # Compute the statistics on an (calendar and water) year basis
-#
-#   2017-01-30 CJS First edition
-
-# Copyright 2017 Province of British Columbia
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and limitations under the License.
 
 annual.stats <- function(station.name=NULL,
                          flow.data=NULL,
@@ -477,6 +463,7 @@ annual.stats <- function(station.name=NULL,
                Q.flow.summary=flow.sum,
                Q.stat.annual=Q.stat,
                Q.stat.annual.trans=Q.stat.trans,
+               Q.lowflows=Q.lowflows,
                "zyp trending method"=zyp.trending,
                Q.zyp.trends=Q.zyp.trends,
                dates.missing.flows=dates.missing.flows,
