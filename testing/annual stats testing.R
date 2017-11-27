a7390aab1f91e2d49bc994df1f005bad5ca808a4
jongoetz <jon.goetz@gov.bc.ca>
2017-11-26 22:17:36 -0800

    added monthly_stats function

--- testing/annual stats testing.R
+++ testing/annual stats testing.R
@@ -9,6 +9,10 @@ low.flows <- fasstr::fasstr_annual_lowflows(HYDAT = "08HB048",rolling_days = c(1
                                             write_table = T)
 low.flows <- dplyr::select(low.flows,-dplyr::contains("Date"))
 
+
+months <- fasstr::fasstr_monthly_stats(HYDAT = "08HB048",percentiles = c(10,25,75,90),
+                                       spread = T,transpose = T)
+
 flowdata=NULL
 HYDAT="08HB048"
 station_name="fasstr"
