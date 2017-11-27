bf85031f66405dda94f7c6fe4e49d75158b558e6
Jon Goetz <JGOETZ@GOVBC>
2017-11-27 09:58:55 -0800

    update

--- testing/annual stats testing.R
+++ testing/annual stats testing.R
@@ -1,17 +1,15 @@
 
 devtools::document()
-install.packages("/Users/jongoetz/Documents/R/fasstr",repos = NULL, type = "source")
-#install.packages("C:/Users/jgoetz/R/fasstr",repos = NULL, type = "source")
+#install.packages("/Users/jongoetz/Documents/R/fasstr",repos = NULL, type = "source")
+install.packages("C:/Users/jgoetz/R/fasstr",repos = NULL, type = "source")
 
 fasstr::fasstr_annual_stats(HYDAT = "08HB048",write_table = T,exclude_years = c(1972:1975),start_year = 1974)
-low.flows <- fasstr::fasstr_annual_lowflows(HYDAT = "08HB048",rolling_days = c(1:10),
-                                            water_year = T,water_year_start = 8,
-                                            write_table = T)
+low.flows <- fasstr::fasstr_annual_lowflows(HYDAT = "08HB048",water_year = T)
 low.flows <- dplyr::select(low.flows,-dplyr::contains("Date"))
 
 
-months <- fasstr::fasstr_monthly_stats(HYDAT = "08HB048",percentiles = c(10,25,75,90),
-                                       spread = T,transpose = T)
+months <- fasstr::fasstr_monthly_stats(HYDAT = "08HB048",percentiles = c(10,25,75,90),water_year = T,water_year_start = 5)
+months2 <- with(months, months[order(Year, Month),])
 
 flowdata=NULL
 HYDAT="08HB048"
@@ -27,3 +25,7 @@ write_table=FALSE
 report_dir="."
 na.rm=list(na.rm.global=FALSE)
 table_nddigits=3
+
+
+
+fasstr::fasstr_add_date_vars(HYDAT = "08HB048")
