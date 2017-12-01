9fe9a4879b1862ce9209cd8c7ba29f2697c3bfee
jongoetz <jon.goetz@gov.bc.ca>
2017-12-01 12:17:56 -0800

    update documentation and formatting

--- R/fasstr_LTMAD.R
+++ R/fasstr_LTMAD.R
@@ -16,7 +16,7 @@
 #'   unless specified.
 #'
 #' @param flowdata Data frame. A data frame of daily mean flow data that includes two columns: a 'Date' column with dates formatted 
-#'    YYYY-MM-DD, and a 'Value' column with the corresponding daily mean flow values in units of cubic metres per second. 
+#'    YYYY-MM-DD, and a numeric 'Value' column with the corresponding daily mean flow values in units of cubic metres per second. 
 #'    Not required if \code{HYDAT} argument is used.
 #' @param HYDAT Character. A seven digit Water Survey of Canada station number (e.g. \code{"08NM116"}) of which to extract daily streamflow 
 #'    data from a HYDAT database. \href{https://github.com/ropensci/tidyhydat}{Installation} of the \code{tidyhydat} package and a HYDAT 
@@ -58,44 +58,40 @@ fasstr_LTMAD <- function(flowdata=NULL,
   #--------------------------------------------------------------
   #  Error checking on the input parameters
   
-  if( !is.null(HYDAT) & !is.null(flowdata))  {
-    stop("must select either flowdata or HYDAT arguments, not both")}
+  if( !is.null(HYDAT) & !is.null(flowdata))           {stop("must select either flowdata or HYDAT arguments, not both")}
   if( is.null(HYDAT)) {
-    if( is.null(flowdata)) {stop("one of flowdata or HYDAT arguments must be set")}
-    if( !is.data.frame(flowdata)) {stop("flowdata arguments is not a data frame")}
-    if( !all(c("Date","Value") %in% names(flowdata))){stop("flowdata data frame doesn't contain the variables 'Date' and 'Value'")}
-    if( !inherits(flowdata$Date[1], "Date")){stop("'Date' column in flowdata data frame is not a date")}
-    if( !is.numeric(flowdata$Value)) {stop("'Value' column in flowdata data frame is not numeric")}
-    if( any(flowdata$Value <0, na.rm=TRUE)) {stop('flowdata cannot have negative values - check your data')}
+    if( is.null(flowdata))                            {stop("one of flowdata or HYDAT arguments must be set")}
+    if( !is.data.frame(flowdata))                     {stop("flowdata arguments is not a data frame")}
+    if( !all(c("Date","Value") %in% names(flowdata))) {stop("flowdata data frame doesn't contain the variables 'Date' and 'Value'")}
+    if( !inherits(flowdata$Date[1], "Date"))          {stop("'Date' column in flowdata data frame is not a date")}
+    if( !is.numeric(flowdata$Value))                  {stop("'Value' column in flowdata data frame is not numeric")}
+    if( any(flowdata$Value <0, na.rm=TRUE))           {stop('flowdata cannot have negative values - check your data')}
   }
   
-  if( !is.logical(water_year))  {stop("water_year parameter must be logical (TRUE/FALSE)")}
-  if( !is.numeric(water_year_start) ) {stop("water_year_start must be a number between 1 and 12 (Jan-Dec)")}
-  if( length(water_year_start)>1) {stop("water_year_start must be a number between 1 and 12 (Jan-Dec)")}
-  if( !water_year_start %in% c(1:12) ) {stop("water_year_start must be an integer between 1 and 12 (Jan-Dec)")}
+  if( !is.logical(water_year))         {stop("water_year argument must be logical (TRUE/FALSE)")}
+  if( !is.numeric(water_year_start) )  {stop("water_year_start argument must be a number between 1 and 12 (Jan-Dec)")}
+  if( length(water_year_start)>1)      {stop("water_year_start argument must be a number between 1 and 12 (Jan-Dec)")}
+  if( !water_year_start %in% c(1:12) ) {stop("water_year_start argument must be an integer between 1 and 12 (Jan-Dec)")}
   
-  if( length(start_year)>1) {stop("only one start_year integer can be selected")}
-  if( !is.null(start_year) ) {if( !start_year %in% c(0:5000) )  {stop("start_year must be an integer")}}
-  if( length(end_year)>1) {stop("only one end_year integer can be selected")}
-  if( !is.null(end_year) ) {if( !end_year %in% c(0:5000) )  {stop("end_year must be an integer")}}
+  if( length(start_year)>1)   {stop("only one start_year value can be selected")}
+  if( !is.null(start_year) )  {if( !start_year %in% c(0:5000) )  {stop("start_year must be an integer")}}
+  if( length(end_year)>1)     {stop("only one end_year value can be selected")}
+  if( !is.null(end_year) )    {if( !end_year %in% c(0:5000) )  {stop("end_year must be an integer")}}
   
-  if( !is.null(excluded_years) ) {if( !all(excluded_years %in% c(0:5000)) )  {stop("excluded_years must be integers (ex. 1999 or c(1999,2000))")}}
+  if( !is.null(excluded_years) ) {
+    if( !all(excluded_years %in% c(0:5000)) ) {stop("excluded_years must be integers (ex. 1999 or c(1999,2000))")}}
   if ( (is.null(start_year) & is.null(end_year) & is.null(excluded_years)) & water_year ) {
-    message("water_year=TRUE ignored; no start_year, end_year, or excluded_years selected to filter the dates")}
+    message("water_year=TRUE ignored; no start_year, end_year, or excluded_years selected to filter dates")}
   
   if( length(percent_MAD)>1) {stop("percent_MAD must be a single number > 0")}
-  if( percent_MAD <=0 ) {stop("percent_MAD must be a single number > 0")}
-  
-  
-  #--------------------------------------------------------------
+  if( percent_MAD <=0 )      {stop("percent_MAD must be a single number > 0")}
+
   # If HYDAT station is listed, check if it exists and make it the flowdata
-  
   if (!is.null(HYDAT)) {
-    if( length(HYDAT)>1 ) {stop("Only one HYDAT station can be selected.")}
-    if (!HYDAT %in% tidyhydat::allstations$STATION_NUMBER) {stop("Station in 'HYDAT' parameter does not exist.")}
+    if( length(HYDAT)>1 )                                  {stop("Only one HYDAT station can be selected.")}
+    if (!HYDAT %in% tidyhydat::allstations$STATION_NUMBER) {stop("Station in 'HYDAT' arguement does not exist.")}
     flowdata <- suppressMessages(tidyhydat::hy_daily_flows(station_number =  HYDAT))
   }
-  
   
   #--------------------------------------------------------------
   # Set the flowdata for analysis
