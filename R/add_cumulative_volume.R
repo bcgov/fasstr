7b3ae76ab9c734021228887936598be6ccac9f68
Jon Goetz <JGOETZ@GOVBC>
2018-02-07 18:47:09 -0800

    testing out new formatting

--- R/add_cumulative_volume.R
+++ R/add_cumulative_volume.R
@@ -12,30 +12,28 @@
 
 #' @title Add daily cumulative volumetric flows on an annual basis
 #'
-#' @description Add a column of daily cumulative volumetric flows on an annual basis to a streamflow dataset. Adds the volumetric discharge 
-#'    from each day with the previous day(s) for each year, in units of cubic metres. The cumulative flows restart every year.
+#' @description Add a column of daily cumulative volumetric flows on an annual basis to a streamflow dataset. Adds the volumetric 
+#'    discharge from each day with the previous day(s) for each year, in units of cubic metres. The cumulative flows restart every year.
 #'
-#' @param data Daily data to be analyzed. Options:
-#' 
-#'    A data frame of daily data that contains columns of dates, values, and (optional) groups (ex. station 
-#'    names/numbers).
-#'    
-#'    A character string vector of seven digit Water Survey of Canada station numbers (e.g. \code{"08NM116"}) of which to 
-#'    extract daily streamflow data from a HYDAT database. Requires \code{tidyhydat} package and a HYDAT database.   
-#' @param dates Column in the \code{data} data frame that contains dates formatted YYYY-MM-DD. Only required if
-#'    using the data frame option of \code{data} and dates column is not named 'Date'. Default \code{Date}. 
-#' @param values Column in the \code{data} data frame that contains numeric flow values, in units of cubic metres per second.
-#'    Only required if using the data frame option of \code{data} and values column is not named 'Value'. Default \code{Value}. 
-#' @param groups Column in the \code{data} data frame that contains unique identifiers for different data sets. 
-#'    Only required if using the data frame option of \code{data} and groups column is not named 'STATION_NUMBER'.
-#'    Function will automatically group by a column named 'STATION_NUMBER' if present. Remove the 'STATION_NUMBER' column or identify 
-#'    another non-existing column name to remove this grouping. Identify another column if desired. Default \code{STATION_NUMBER}. 
+#' @param data A data frame of daily data that contains columns of dates, values, and (optional) groups (ex. station 
+#'    names/numbers). Leave blank if using \code{station_number} argument.
+#' @param dates Column in \code{data} that contains dates formatted YYYY-MM-DD. Only required if dates column name is not '
+#'    Date' (default). Leave blank if using \code{station_number} argument.
+#' @param values Column in \code{data} that contains numeric flow values, in units of cubic metres per second. 
+#'    Only required if values column name is not 'Value' (default). Leave blank if using \code{station_number} argument.
+#' @param groups Column in \code{data} that contains unique identifiers for different data sets. Only required if groups 
+#'    column name is not 'STATION_NUMBER'. Function will automatically group by a column named 'STATION_NUMBER' if present.
+#'    Remove the 'STATION_NUMBER' column or identify another non-existing column name to remove this grouping.
+#'    Leave blank if using \code{station_number} argument.
+#' @param station_number A character string vector of seven digit Water Survey of Canada station numbers (e.g. \code{"08NM116"}) of 
+#'    which to extract daily streamflow data from a HYDAT database. Requires \code{tidyhydat} package and a HYDAT database. 
+#'    Leave blank if using \code{data} argument.
 #' @param water_year Logical value indicating whether to use water years to group data instead of calendar years. Water years 
 #'    are designated by the year in which they end. Default \code{FALSE}.
 #' @param water_year_start Numeric value indicating the month of the start of the water year. Used if \code{water_year = TRUE}. 
 #'    Default \code{10}.
 #' 
-#' @return A data frame of the source data with an additional column:
+#' @return A tibble data frame of the source data with an additional column:
 #'   \item{Cumul_Volume_m3}{cumulative volumetric flows for each day for each year, in units of cubic metres}
 #'   
 #' @examples
@@ -69,7 +67,7 @@ add_cumulative_volume <- function(data = NULL,
   orig_cols <- names(flow_data)
   
   # Get groups of flow_data to return after
-  flow_data_groups <- dplyr::group_vars(flow_data)
+  orig_groups <- dplyr::group_vars(flow_data)
   
   # Check and rename columns
   flow_data <- format_all_cols(data = flow_data,
@@ -88,7 +86,7 @@ add_cumulative_volume <- function(data = NULL,
   ## -----------------
   
   # Fill in missing dates to ensure all years are covered
-  flow_data_temp <- fill_missing_dates(data = flow_data)
+  flow_data_temp <- fill_missing_dates(data = flow_data, water_year = water_year, water_year_start = water_year_start)
   flow_data_temp <- add_date_variables(data = flow_data_temp, water_year = water_year, water_year_start = water_year_start)
   
   # Set selected year-type column for analysis
@@ -120,13 +118,13 @@ add_cumulative_volume <- function(data = NULL,
   if("Cumul_Volume_m3" %in% orig_cols){
     flow_data <- merge(flow_data, flow_data_temp, by = c("STATION_NUMBER", "Date"), all.x = TRUE)
     flow_data$Cumul_Volume_m3 <- flow_data$Cumul_Volume_m3.y
-    flow_data <- dplyr::select(flow_data,-Cumul_Volume_m3.y,-Cumul_Volume_m3.x)
+    flow_data <- dplyr::select(flow_data, -Cumul_Volume_m3.y, -Cumul_Volume_m3.x)
   } else {
     flow_data <- merge(flow_data, flow_data_temp, by = c("STATION_NUMBER", "Date"), all.x = TRUE)
   }
   
-  ## Reformat to original data
-  ## -------------------------
+  ## Reformat to original names and groups
+  ## -------------------------------------
   
   # Return the original names of the Date and Value columns
   names(flow_data)[names(flow_data) == "STATION_NUMBER"] <- as.character(substitute(groups))
@@ -141,7 +139,7 @@ add_cumulative_volume <- function(data = NULL,
   }
   
   # Regroup by the original groups
-  flow_data <- dplyr::group_by_at(flow_data, dplyr::vars(flow_data_groups))
+  flow_data <- dplyr::group_by_at(flow_data, dplyr::vars(orig_groups))
   
   dplyr::as_tibble(flow_data)
   
