52073529afe473c11cd1f44a1fc418efe8144d3d
Jon Goetz <JGOETZ@GOVBC>
2018-03-19 11:17:42 -0700

    Updated to match new style

--- R/plot_annual_trends.R
+++ R/plot_annual_trends.R
@@ -11,14 +11,14 @@
 # See the License for the specific language governing permissions and limitations under the License.
 
 
-#' @title Plot prewhitened nonlinear trends on streamflow data
+#' @title Plot prewhitened nonlinear annual trends on streamflow data
 #'
-#' @description Plots the annual data and significant trend from the results from all statistics from a compute_annual_trends()
-#'    analysis.
+#' @description Plots the annual data and significance trend from results from a compute_annual_trends() analysis data frame.
 #' 
-#' @param trends_results Data frame of results from the compute_annual_trends() function, with original data included
-#'    (incl_data = TRUE). The first column must be 'Statistics' with the following columns with results and then data.
-#' @param zyp_alpha Numeric value of the significance level of when to plot a trend line. Set to NA if no line required.
+#' @param trends_results A data frame of results from the compute_annual_trends() function, with original data included
+#'    (compute_annual_trends(incl_data = TRUE)). Uses the only the "Statistics" and zyp results columns, along with the 
+#'    annual data to plot.
+#' @param zyp_alpha Numeric value of the significance level (ex. 0.05) of when to plot a trend line. Leave blank for no line.
 #' 
 #' @return A list of ggplot2 objects with plots of trended annual statistics, as the name of each object, with a trend line
 #'    plotted if less than the zyp_alpha provided.
@@ -26,9 +26,10 @@
 #' @examples
 #' \dontrun{
 #' 
-#' trends <- compute_annual_trends("08NM116",
+#' trends <- compute_annual_trends(station_number = "08NM116",
 #'                                 zyp_method = "yuepilon",
-#'                                 start_year = 1980, end_year = 2010)
+#'                                 start_year = 1980, 
+#'                                 end_year = 2010)
 #'  
 #' plot_annual_trends(trends_results = trends,
 #'                    zyp_alpha = 0.05)
@@ -39,35 +40,43 @@
 
 
 plot_annual_trends <- function(trends_results = NULL,
-                               zyp_alpha = 0.05){          
+                               zyp_alpha = NA){          
   
-  ## CHECKS ON DATA 
-  ##---------------
+  ## ARGUMENT CHECKS
+  ## ---------------
   
-  if(is.null(trends_results))        stop("Must provide a data frame of compute_annual_trends() results with incl_data = TRUE.")
-  if(!is.data.frame(trends_results)) stop("Must provide a data frame of compute_annual_trends() results with incl_data = TRUE.")
+  if(is.null(trends_results))        
+    stop("Must provide a data frame of compute_annual_trends() results with incl_data = TRUE.", call. = FALSE)
+  if(!is.data.frame(trends_results)) 
+    stop("Must provide a data frame of compute_annual_trends() results with incl_data = TRUE.", call. = FALSE)
   
   if(!all(c("lbound", "trend", "trendp", "tau", "sig", "linear", "n_years") %in% names(trends_results)))
-    stop("Must provide a data frame of compute_annual_trends() results with incl_data = TRUE.")
+    stop("Must provide a data frame of compute_annual_trends() results with incl_data = TRUE.", call. = FALSE)
   
-  if(!is.na(zyp_alpha) & !is.numeric(zyp_alpha) )              stop("zyp_alpha must be numeric")
-  if(!is.na(zyp_alpha) & !all(zyp_alpha > 0 & zyp_alpha < 1))  stop("timing_percent must be > 0 and < 1)")
+  if(!is.na(zyp_alpha) & !is.numeric(zyp_alpha) )              
+    stop("zyp_alpha must be numeric.", call. = FALSE)
+  if(!is.na(zyp_alpha) & !all(zyp_alpha > 0 & zyp_alpha < 1))  
+    stop("timing_percent must be > 0 and < 1)", call. = FALSE)
   
   
   ## EXTRACT DATA AND RESULTS
   ## ------------------------
   
-  trends_data <- trends_results[,c(1,20:ncol(trends_results))]
+  if ("STATION_NUMBER" %in% colnames(trends_results)) {
+    trends_results <- dplyr::select(trends_results, -STATION_NUMBER)
+  }
+  
+  trends_data <- trends_results[,c(1, 20:ncol(trends_results))]
   trends_results <- trends_results[,c(1:19)]
   
   
   ## PLOT TRENDS
   ## -----------
-
+  
   # Create the list to place all plots
   trends_plots <- list()
-
- 
+  
+  
   # Set data for plotting
   trends_data <- tidyr::gather(trends_data, Year, Value, -1)
   trends_data <- dplyr::mutate(trends_data, Year = as.numeric(Year))
@@ -79,7 +88,7 @@ plot_annual_trends <- function(trends_results = NULL,
                                Units = replace(Units, grepl("DoY", Statistic), "Day of Year"),
                                Units = replace(Units, grepl("Days", Statistic), "Number of Days"))
   
-
+  
   # Loop through each statistic and plot the annual data, add trendline if < zyp_alpha
   for (stat in unique(trends_results$Statistic)){
     # Filter for metric
@@ -105,18 +114,14 @@ plot_annual_trends <- function(trends_results = NULL,
       trends_plot <- trends_plot +
         ggplot2::geom_abline(slope = trends_results_stat$trend, intercept = int, colour = "red")
     }
-
+    
     trends_plots[[paste0(stat, "-trends")]] <- trends_plot
-
-
-
+    
+    
+    
   }
-
-
-
-
+  
   return(trends_plots)
-
-
+  
 }
 
