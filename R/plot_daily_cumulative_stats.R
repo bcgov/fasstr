b2ae0dd7f59f14d090a09d848ef3e45d62c568ba
Jon Goetz <JGOETZ@GOVBC>
2018-04-04 16:55:41 -0700

    multiple plotting

--- R/plot_daily_cumulative_stats.R
+++ R/plot_daily_cumulative_stats.R
@@ -56,6 +56,7 @@ plot_daily_cumulative_stats <- function(data = NULL,
                                         end_year = 9999,
                                         exclude_years = NULL, 
                                         log_discharge = FALSE,
+                                        include_title = FALSE,
                                         include_year = NULL){
   
   ## ARGUMENT CHECKS
@@ -63,8 +64,7 @@ plot_daily_cumulative_stats <- function(data = NULL,
   
   log_discharge_checks(log_discharge) 
   include_year_checks(include_year)
-  one_station_number_stop(station_number)
-  
+  include_title_checks(include_title)  
   
   
   ## FLOW DATA CHECKS AND FORMATTING
@@ -74,26 +74,13 @@ plot_daily_cumulative_stats <- function(data = NULL,
   flow_data <- flowdata_import(data = data, station_number = station_number)
   
   # Check and rename columns
-  flow_data <- format_plot_cols(data = flow_data, 
-                                dates = as.character(substitute(dates)),
-                                values = as.character(substitute(values)),
-                                groups = as.character(substitute(groups)),
-                                use_groups = TRUE)
+  # Check and rename columns
+  flow_data <- format_all_cols(data = flow_data,
+                               dates = as.character(substitute(dates)),
+                               values = as.character(substitute(values)),
+                               groups = as.character(substitute(groups)),
+                               rm_other_cols = TRUE)
   
-  
-  
-  ## CALC STATS
-  ## ----------
-  
-  daily_stats <- fasstr::calc_daily_cumulative_stats(data = flow_data,
-                                                     percentiles = c(5,25,75,95),
-                                                     use_yield = use_yield, 
-                                                     basin_area = ifelse(use_yield, basin_area, 0),
-                                                     water_year = water_year,
-                                                     water_year_start = water_year_start,
-                                                     start_year = start_year,
-                                                     end_year = end_year,
-                                                     exclude_years = exclude_years)
   
   if (water_year) {
     # Create origin date to apply to flow_data and Q_daily later on
@@ -113,53 +100,23 @@ plot_daily_cumulative_stats <- function(data = NULL,
     origin_date <- as.Date("1899-12-31")
   }
   
-  daily_stats <- dplyr::mutate(daily_stats, Date = as.Date(DayofYear, origin = origin_date))
-  
-  
-  ## PLOT STATS
+  ## CALC STATS
   ## ----------
   
-  # Create the daily stats plots
-  daily_stats_plot <- ggplot2::ggplot(daily_stats, ggplot2::aes(x = Date)) +
-    ggplot2::geom_ribbon(ggplot2::aes(ymin = Minimum, ymax = P5, fill = "Min-5th Percentile")) +
-    ggplot2::geom_ribbon(ggplot2::aes(ymin = P5, ymax = P25, fill = "5th-25th Percentile")) +
-    ggplot2::geom_ribbon(ggplot2::aes(ymin = P25, ymax = P75, fill = "25th-75th Percentile")) +
-    ggplot2::geom_ribbon(ggplot2::aes(ymin = P75, ymax = P95, fill = "75th-95th Percentile")) +
-    ggplot2::geom_ribbon(ggplot2::aes(ymin = P95, ymax = Maximum, fill = "95th Percentile-Max")) +
-    ggplot2::geom_line(ggplot2::aes(y = Median, colour = "Median"), size = .5) +
-    ggplot2::geom_line(ggplot2::aes(y = Mean, colour = "Mean"), size = .5) +
-    ggplot2::scale_fill_manual(values = c("Min-5th Percentile" = "orange" , "5th-25th Percentile" = "yellow",
-                                          "25th-75th Percentile" = "skyblue1", "75th-95th Percentile" = "dodgerblue2",
-                                          "95th Percentile-Max" = "royalblue4")) +
-    ggplot2::scale_color_manual(values = c("Median" = "purple3", "Mean" = "springgreen4")) +
-    {if (!log_discharge) ggplot2::scale_y_continuous(expand = c(0, 0))} +
-    {if (log_discharge) ggplot2::scale_y_log10(expand = c(0, 0))} +
-    {if (log_discharge) ggplot2::annotation_logticks(base= 10, "left", colour = "grey25", size = 0.3,
-                                                     short = ggplot2::unit(.07, "cm"), mid = ggplot2::unit(.15, "cm"),
-                                                     long = ggplot2::unit(.2, "cm"))} +
-    ggplot2::scale_x_date(date_labels = "%b", date_breaks = "1 month",
-                          limits = as.Date(c(NA, as.character(max(daily_stats$Date)))), expand=c(0, 0)) +
-    ggplot2::xlab("Day of Year")+
-    {if (!use_yield) ggplot2::ylab("Cumulative Discharge (cubic metres)")} +
-    {if (use_yield) ggplot2::ylab("Cumulative Runoff Yield (mm)")} +
-    ggplot2::theme_bw() +
-    ggplot2::labs(color = 'Daily Statistics', fill = "Daily Ranges") +  
-    ggplot2::theme(axis.text=ggplot2::element_text(size = 10, colour = "grey25"),
-                   axis.title=ggplot2::element_text(size = 12, colour = "grey25"),
-                   axis.title.y=ggplot2::element_text(margin = ggplot2::margin(0,0,0,0)),
-                   axis.ticks = ggplot2::element_line(size = .1, colour = "grey25"),
-                   axis.ticks.length=ggplot2::unit(0.05, "cm"),
-                   panel.border = ggplot2::element_rect(colour = "black", fill = NA, size = 1),
-                   panel.grid.minor = ggplot2::element_blank(),
-                   panel.grid.major = ggplot2::element_line(size = .1),
-                   panel.background = ggplot2::element_rect(fill = "grey94"),
-                   legend.text = ggplot2::element_text(size = 9, colour = "grey25"),
-                   legend.box = "vertical",
-                   legend.justification = "top",
-                   legend.key.size = ggplot2::unit(0.4, "cm"),
-                   legend.spacing = ggplot2::unit(0, "cm")) +
-    ggplot2::guides(colour = ggplot2::guide_legend(order = 1), fill = ggplot2::guide_legend(order = 2))
+  daily_stats <- calc_daily_cumulative_stats(data = flow_data,
+                                             percentiles = c(5,25,75,95),
+                                             use_yield = use_yield, 
+                                             basin_area = ifelse(use_yield, basin_area, 0),
+                                             water_year = water_year,
+                                             water_year_start = water_year_start,
+                                             start_year = start_year,
+                                             end_year = end_year,
+                                             exclude_years = exclude_years)
   
+ 
+  
+  daily_stats <- dplyr::mutate(daily_stats, Date = as.Date(DayofYear, origin = origin_date))
+  daily_stats <- dplyr::mutate(daily_stats, AnalysisDate = Date)
   
   
   ## ADD YEAR IF SELECTED
@@ -167,47 +124,116 @@ plot_daily_cumulative_stats <- function(data = NULL,
   
   if(!is.null(include_year)){
     
-    flow_data <- fill_missing_dates(data = flow_data, water_year = water_year, water_year_start = water_year_start)
-    flow_data <- add_date_variables(data = flow_data, water_year = water_year, water_year_start = water_year_start)
+    year_data <- fill_missing_dates(data = flow_data, water_year = water_year, water_year_start = water_year_start)
+    year_data <- add_date_variables(data = year_data, water_year = water_year, water_year_start = water_year_start)
     
     # Add cumulative flows
     if (use_yield){
-      flow_data <- add_cumulative_yield(data = flow_data, water_year = water_year, water_year_start = water_year_start, basin_area = basin_area)
-      flow_data$Cumul_Flow <- flow_data$Cumul_Yield_mm
+      year_data <- add_cumulative_yield(data = year_data, water_year = water_year, water_year_start = water_year_start, basin_area = basin_area)
+      year_data$Cumul_Flow <- year_data$Cumul_Yield_mm
     } else {
-      flow_data <- add_cumulative_volume(data = flow_data, water_year = water_year, water_year_start = water_year_start)
-      flow_data$Cumul_Flow <- flow_data$Cumul_Volume_m3
+      year_data <- add_cumulative_volume(data = year_data, water_year = water_year, water_year_start = water_year_start)
+      year_data$Cumul_Flow <- year_data$Cumul_Volume_m3
     }
     
     if (water_year) {
-      flow_data$AnalysisYear <- flow_data$WaterYear
-      flow_data$AnalysisDoY <- flow_data$WaterDayofYear
+      year_data$AnalysisYear <- year_data$WaterYear
+      year_data$AnalysisDoY <- year_data$WaterDayofYear
     }  else {
-      flow_data$AnalysisYear <- flow_data$Year
-      flow_data$AnalysisDoY <- flow_data$DayofYear
+      year_data$AnalysisYear <- year_data$Year
+      year_data$AnalysisDoY <- year_data$DayofYear
     }
-    flow_data <- dplyr::mutate(flow_data, AnalysisDate = as.Date(AnalysisDoY, origin = origin_date))
-    flow_data <- dplyr::filter(flow_data, AnalysisYear >= start_year & AnalysisYear <= end_year)
-    flow_data <- dplyr::filter(flow_data, !(AnalysisYear %in% exclude_years))
-    flow_data <- dplyr::filter(flow_data, AnalysisDoY < 366)
+    year_data <- dplyr::mutate(year_data, AnalysisDate = as.Date(AnalysisDoY, origin = origin_date))
+    year_data <- dplyr::filter(year_data, AnalysisYear >= start_year & AnalysisYear <= end_year)
+    year_data <- dplyr::filter(year_data, !(AnalysisYear %in% exclude_years))
+    year_data <- dplyr::filter(year_data, AnalysisDoY < 366)
     
-    if(!include_year %in% flow_data$AnalysisYear) stop(paste0("Year in include_year does not exist. Please choose a year between ",
-                                                              min(flow_data$AnalysisYear), " and ", max(flow_data$AnalysisYear), "."), 
-                                                       call. = FALSE)
-    flow_data <- dplyr::filter(flow_data, AnalysisYear == include_year)
     
-    # Plot the year
-    suppressMessages(
-      daily_stats_plot <- daily_stats_plot +
-        ggplot2::geom_line(data = flow_data, ggplot2::aes(x = AnalysisDate, y = Cumul_Flow, colour = "yr.colour"), size = 0.5) +
-        ggplot2::scale_color_manual(values = c("Mean" = "paleturquoise", "Median" = "dodgerblue4", "yr.colour" = "red"),
-                                    labels = c("Mean", "Median", paste0(include_year, " Flows")))
-    )
-  }
+    year_data <- dplyr::filter(year_data, AnalysisYear == include_year)
+    
+    year_data <- dplyr::select(year_data, STATION_NUMBER, AnalysisDate, Cumul_Flow)
+    
+    # Add the daily data from include_year to the daily stats
+    daily_stats <- dplyr::left_join(daily_stats, year_data, by = c("STATION_NUMBER", "AnalysisDate"))
+    
+    # Warning if all daily values are NA from the include_year
+    for (stn in unique(daily_stats$STATION_NUMBER)) {
+      year_test <- dplyr::filter(daily_stats, STATION_NUMBER == stn)
+      
+      if(all(is.na(daily_stats$Cumul_Flow)))
+        warning("Daily data does not exist for the year listed in include_year and was not plotted.", call. = FALSE)
+    }
+    
+  } 
+    
+    
+    
+  ## PLOT STATS
+  ## ----------
   
-  suppressWarnings(
-    daily_stats_plot
-  )
+  # Create the daily stats plots
+  daily_plots <- dplyr::group_by(daily_stats, STATION_NUMBER)
+  daily_plots <- tidyr::nest(daily_plots)
+  daily_plots <- dplyr::mutate(daily_plots,
+                               plot = purrr::map2(data, STATION_NUMBER,
+       ~suppressMessages(
+         suppressWarnings(
+           ggplot2::ggplot(., ggplot2::aes(x = AnalysisDate)) +
+             ggplot2::geom_ribbon(ggplot2::aes(ymin = Minimum, ymax = P5, fill = "Min-5th Percentile")) +
+             ggplot2::geom_ribbon(ggplot2::aes(ymin = P5, ymax = P25, fill = "5th-25th Percentile")) +
+             ggplot2::geom_ribbon(ggplot2::aes(ymin = P25, ymax = P75, fill = "25th-75th Percentile")) +
+             ggplot2::geom_ribbon(ggplot2::aes(ymin = P75, ymax = P95, fill = "75th-95th Percentile")) +
+             ggplot2::geom_ribbon(ggplot2::aes(ymin = P95, ymax = Maximum, fill = "95th Percentile-Max")) +
+             ggplot2::geom_line(ggplot2::aes(y = Median, colour = "Median"), size = .5) +
+             ggplot2::geom_line(ggplot2::aes(y = Mean, colour = "Mean"), size = .5) +
+             ggplot2::scale_fill_manual(values = c("Min-5th Percentile" = "orange" , "5th-25th Percentile" = "yellow",
+                                                   "25th-75th Percentile" = "skyblue1", "75th-95th Percentile" = "dodgerblue2",
+                                                   "95th Percentile-Max" = "royalblue4")) +
+             ggplot2::scale_color_manual(values = c("Median" = "purple3", "Mean" = "springgreen4")) +
+             {if (!log_discharge) ggplot2::scale_y_continuous(expand = c(0, 0))} +
+             {if (log_discharge) ggplot2::scale_y_log10(expand = c(0, 0))} +
+             {if (log_discharge) ggplot2::annotation_logticks(base= 10, "left", colour = "grey25", size = 0.3,
+                                                              short = ggplot2::unit(.07, "cm"), mid = ggplot2::unit(.15, "cm"),
+                                                              long = ggplot2::unit(.2, "cm"))} +
+             ggplot2::scale_x_date(date_labels = "%b", date_breaks = "1 month",
+                                   limits = as.Date(c(NA, as.character(max(daily_stats$AnalysisDate)))), expand=c(0, 0)) +
+             ggplot2::xlab("Day of Year")+
+             {if (!use_yield) ggplot2::ylab("Cumulative Discharge (cubic metres)")} +
+             {if (use_yield) ggplot2::ylab("Cumulative Runoff Yield (mm)")} +
+             ggplot2::theme_bw() +
+             ggplot2::labs(color = 'Daily Statistics', fill = "Daily Ranges") +
+             {if (include_title & .y != "XXXXXXX") ggplot2::labs(color = paste0(.y,'\n \nDaily Statistics'), fill = "Daily Ranges") } +    
+             ggplot2::theme(axis.text=ggplot2::element_text(size = 10, colour = "grey25"),
+                            axis.title=ggplot2::element_text(size = 12, colour = "grey25"),
+                            axis.title.y=ggplot2::element_text(margin = ggplot2::margin(0,0,0,0)),
+                            axis.ticks = ggplot2::element_line(size = .1, colour = "grey25"),
+                            axis.ticks.length=ggplot2::unit(0.05, "cm"),
+                            panel.border = ggplot2::element_rect(colour = "black", fill = NA, size = 1),
+                            panel.grid.minor = ggplot2::element_blank(),
+                            panel.grid.major = ggplot2::element_line(size = .1),
+                            panel.background = ggplot2::element_rect(fill = "grey94"),
+                            legend.text = ggplot2::element_text(size = 9, colour = "grey25"),
+                            legend.box = "vertical",
+                            legend.justification = "top",
+                            legend.key.size = ggplot2::unit(0.4, "cm"),
+                            legend.spacing = ggplot2::unit(0, "cm")) +
+             ggplot2::guides(colour = ggplot2::guide_legend(order = 1), fill = ggplot2::guide_legend(order = 2)) +
+             {if (is.numeric(include_year)) ggplot2::geom_line(ggplot2::aes(y = Cumul_Flow, colour = "yr.colour"), size = 0.5) } +
+             {if (is.numeric(include_year)) ggplot2::scale_color_manual(values = c("Mean" = "paleturquoise", "Median" = "dodgerblue4", "yr.colour" = "red"),
+                                                                        labels = c("Mean", "Median", paste0(include_year, " Flows"))) }
+         ))))
+
+
+
+  # Create a list of named plots extracted from the tibble
+  plots <- daily_plots$plot
+  if (nrow(daily_plots) == 1) {
+    names(plots) <- "Daily_Cumulative_Stats"
+  } else {
+    names(plots) <- paste0(daily_plots$STATION_NUMBER, "_Daily_Cumulative_Stats")
+  }
+
+  plots
   
 }
 
