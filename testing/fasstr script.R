94987036f0530f930860ab933fc5cf4cc2c81188
Jon Goetz <JGOETZ@GOVBC>
2018-04-05 16:51:27 -0700

    script jsut writes everything, no creating of tibbles or plots

--- testing/fasstr script.R
+++ testing/fasstr script.R
@@ -4,6 +4,9 @@
 #install.packages("C:/Users/jgoetz/R/fasstr",repos = NULL, type = "source")
 
 
+# Writes all data and plots
+
+
 library(fasstr)
 # Parameters
 stn_number <- "08NM116"
@@ -12,118 +15,149 @@ end_year = 2000 #NULL
 
 
 folder <- "testing/MissionCreek/"
-dir.create(path = folder)
+dir.create(path = folder, showWarnings = FALSE)
 
 
 ### Time series
 ##########################
 timeseriesfolder <- "1-TimeSeries/"
-dir.create(path=paste0(folder,timeseriesfolder))
+dir.create(path=paste0(folder,timeseriesfolder), showWarnings = FALSE)
 
-timeseries <- fill_missing_dates(data = stn_number)
-timeseries <- add_date_variables(timeseries)
-timeseries <- add_rolling_means(timeseries)
-write_flow_data(timeseries, file = paste0(folder, timeseriesfolder, "daily_record.csv"))
-timeseries_plot <- plot_flow_data(data = stn_number, start_year = start_year,end_year = end_year)
-timeseries_annual_plot <- plot_flow_data(data = stn_number, start_year = start_year,end_year = end_year,
-                                         plot_by_year = TRUE)
+timeseries <- fill_missing_dates(station_number = stn_number)
+timeseries <- add_date_variables(data = timeseries)
+timeseries <- add_rolling_means(data = timeseries)
 
 
-flow_summary <- screen_flow_data(timeseries,start_year = start_year,end_year = end_year)
-summary_plot <- plot_data_screening(timeseries,start_year = start_year,end_year = end_year)
-missing_plot <- plot_missing_dates(timeseries,start_year = start_year,end_year = end_year)
+write_flow_data(data = timeseries, 
+                file = paste0(folder, timeseriesfolder, "daily_record.xlsx"))
 
+write_results(data = screen_flow_data(data = timeseries, start_year = start_year, end_year = end_year), 
+              file = paste0(folder, timeseriesfolder, "flow_summary.xlsx"))
+
+write_plots(plots = c(plot_flow_data(data = timeseries, start_year = start_year,end_year = end_year,plot_by_year = TRUE),
+                      plot_flow_data(data = timeseries, start_year = start_year,end_year = end_year),
+                      plot_data_screening(data = timeseries, start_year = start_year, end_year = end_year),
+                      plot_missing_dates(data = timeseries, start_year = start_year, end_year = end_year)),
+            foldername = paste0(folder, timeseriesfolder),
+            type = "pdf",
+            width = 8.5,
+            height = 4)
 
 
 ### Long-term Stats
 ##########################
 longtermfolder <- "2-Longterm/"
-dir.create(path=paste0(folder,longtermfolder))
+dir.create(path=paste0(folder,longtermfolder), showWarnings = FALSE)
 
-longterm_table <- calc_longterm_stats(timeseries, start_year = start_year,end_year = end_year, percentiles = 1:99)
-longterm_plot <- plot_longterm_stats(timeseries,start_year = start_year,end_year = end_year)
+write_results(data = calc_longterm_stats(timeseries, start_year = start_year,end_year = end_year, percentiles = 1:99, transpose = T), 
+              file = paste0(folder, longtermfolder, "longterm_stats.xlsx"))
 
-flow_curves <- plot_flow_duration(timeseries,start_year = start_year,end_year = end_year)
-
-
+write_plots(plots = c(plot_longterm_stats(timeseries,start_year = start_year,end_year = end_year),
+                      plot_flow_duration(timeseries,start_year = start_year,end_year = end_year)), 
+            foldername = paste0(folder, longtermfolder), 
+            type = "pdf",
+            width = 8.5,
+            height = 4)
 
 ### Annual Stats
 ##########################
 annualfolder <- "3-Annual/"
-dir.create(path=paste0(folder,annualfolder))
+dir.create(path=paste0(folder,annualfolder), showWarnings = FALSE)
 
-annual_stats <- calc_annual_stats(timeseries,start_year = start_year,end_year = end_year)
-annual_total <- calc_annual_cumulative_stats(timeseries,start_year = start_year,end_year = end_year)
-annual_yield <- calc_annual_cumulative_stats(timeseries,start_year = start_year,end_year = end_year, use_yield = T)
-annual_timing <- calc_annual_flow_timing(timeseries,start_year = start_year,end_year = end_year)# DAYS OUTSIDE NORMAL
-annual_normal <- calc_annual_outside_normal(timeseries,start_year = start_year,end_year = end_year)
+write_results(data = calc_annual_stats(timeseries,start_year = start_year,end_year = end_year), 
+              file = paste0(folder, annualfolder, "annual_stats.xlsx"))
+write_results(data = calc_annual_cumulative_stats(timeseries,start_year = start_year,end_year = end_year), 
+              file = paste0(folder, annualfolder, "annual_cumulative_flows.xlsx"))
+write_results(data = calc_annual_cumulative_stats(timeseries,start_year = start_year,end_year = end_year, use_yield = T), 
+              file = paste0(folder, annualfolder, "annual_yield.xlsx"))
+write_results(data = calc_annual_flow_timing(timeseries,start_year = start_year,end_year = end_year),# DAYS OUTSIDE NORMAL, 
+              file = paste0(folder, annualfolder, "annual_flow_timing.xlsx"))
+write_results(data = calc_annual_outside_normal(timeseries,start_year = start_year,end_year = end_year), 
+              file = paste0(folder, annualfolder, "annual_normal_days.xlsx"))
+write_results(data = calc_annual_lowflows(timeseries,start_year = start_year,end_year = end_year), 
+              file = paste0(folder, annualfolder, "annual_lowflows.xlsx"))
 
+write_plots(plots = c(plot_annual_stats(timeseries, start_year = start_year, end_year = end_year),
+                      plot_annual_cumulative_stats(timeseries, start_year = start_year, end_year = end_year, incl_seasons = T),
+                      plot_annual_cumulative_stats(timeseries, start_year = start_year, end_year = end_year, incl_seasons = T, use_yield = T),
+                      plot_annual_flow_timing(timeseries, start_year = start_year, end_year = end_year),
+                      plot_annual_outside_normal(timeseries, start_year = start_year, end_year = end_year),
+                      plot_annual_lowflows(timeseries, start_year = start_year, end_year = end_year)
+                      ), 
+            foldername = paste0(folder, annualfolder), 
+            type = "pdf",
+            width = 8.5,
+            height = 4)
 
 ### Monthly Stats
 ##########################
 monthfolder <- "4-Month/"
-dir.create(path=paste0(folder,monthfolder))
+dir.create(path=paste0(folder,monthfolder), showWarnings = FALSE)
 
 # MONTHLY STATS
-monthly_stats <- calc_monthly_stats(timeseries,start_year = start_year,end_year = end_year)
-monthly_total <- calc_monthly_cumulative_stats(timeseries,start_year = start_year,end_year = end_year)
-monthly_yield <- calc_monthly_cumulative_stats(timeseries,start_year = start_year,end_year = end_year, use_yield = T)
+write_results(data = calc_monthly_stats(timeseries,start_year = start_year,end_year = end_year), 
+              file = paste0(folder, monthfolder, "monthly_stats.xlsx"))
+write_results(data = calc_monthly_cumulative_stats(timeseries,start_year = start_year,end_year = end_year), 
+              file = paste0(folder, monthfolder, "monthly_cumulative.xlsx"))
+write_results(data =  calc_monthly_cumulative_stats(timeseries,start_year = start_year,end_year = end_year, use_yield = T), 
+              file = paste0(folder, monthfolder, "monthly_yield.xlsx"))
 
 
-
+write_plots(plots = c(plot_monthly_stats(timeseries, start_year = start_year, end_year = end_year),
+                      plot_monthly_cumulative_stats(timeseries, start_year = start_year, end_year = end_year),
+                      plot_monthly_cumulative_stats(timeseries, start_year = start_year, end_year = end_year, use_yield = T)), 
+            foldername = paste0(folder, monthfolder), 
+            type = "pdf",
+            width = 8.5,
+            height = 4)
 
 ### Daily Stats
 ##########################
 dailyfolder <- "5-Daily/"
-dir.create(path=paste0(folder,dailyfolder))
+dir.create(path=paste0(folder,dailyfolder), showWarnings = FALSE)
 
-daily_table <- fasstr_daily_stats(flowdata=timeseries,
-                                  #HYDAT = stn_number,
-                                  write_table = T,report_dir = paste0(folder,dailyfolder),start_year = start_year,end_year = end_year)
-daily_plots <- fasstr_daily_stats_plots(flowdata=timeseries,
-                                        #HYDAT = stn_number,
-                                        write_plot = T,report_dir = paste0(folder,dailyfolder),log_discharge = T,start_year = start_year,end_year = end_year)
+write_results(data = calc_daily_stats(data = timeseries,start_year = start_year,end_year = end_year), 
+              file = paste0(folder, dailyfolder, "daily_stats.xlsx"))
+write_results(data = calc_daily_cumulative_stats(data=timeseries,start_year = start_year,end_year = end_year), 
+              file = paste0(folder, dailyfolder, "daily_cumulative_volume_stats.xlsx"))
+write_results(data =  calc_daily_cumulative_stats(data=timeseries,start_year = start_year,end_year = end_year, use_yield = T), 
+              file = paste0(folder, dailyfolder, "daily_cumulative_yield_stats.xlsx"))
 
-cumulative_table <- fasstr_daily_cumulative_stats(flowdata=timeseries,
-                                                  #HYDAT = stn_number,
-                                                  write_table = T,report_dir = paste0(folder,dailyfolder),start_year = start_year,end_year = end_year)
-cumulative_plots <- fasstr_daily_cumulative_plots(flowdata=timeseries,
-                                                  #HYDAT = stn_number,
-                                                  write_plot = T,report_dir = paste0(folder,dailyfolder),start_year = start_year,end_year = end_year)
-
-
-
-### Low Flows
-##########################
-lowflowfolder <- "6-LowFlows/"
-dir.create(path=paste0(folder,lowflowfolder))
-
-# LOW FLOWS
+write_plots(plots = c(plot_daily_stats(data = timeseries,start_year = start_year,end_year = end_year),
+                      plot_daily_cumulative_stats(data = timeseries,start_year = start_year,end_year = end_year),
+                      plot_daily_cumulative_stats(data = timeseries,start_year = start_year,end_year = end_year, use_yield = T)), 
+            foldername = paste0(folder, dailyfolder), 
+            type = "pdf",
+            width = 8.5,
+            height = 4)
 
 
 ### Low Flow Frequency
 ##########################
-freqfolder <- "7-LowFlowFrequencies/"
-dir.create(path=paste0(folder,freqfolder))
+# freqfolder <- "6-LowFlowFrequencies/"
+# dir.create(path=paste0(folder,freqfolder), showWarnings = FALSE)
+# 
+# lowflow_results <- fasstr_annual_freq_analysis(flowdata=timeseries,
+#                                                #HYDAT = stn_number,
+#                                                report_dir = paste0(folder,freqfolder),start_year = start_year,end_year = end_year)
+# 
+# 
+# ### Annual Trending
+# ##########################
+trendingfolder <- "7-Trending/"
+dir.create(path=paste0(folder,trendingfolder), showWarnings = FALSE)
 
-lowflow_results <- fasstr_annual_freq_analysis(flowdata=timeseries,
-                                               #HYDAT = stn_number,
-                                               report_dir = paste0(folder,freqfolder),start_year = start_year,end_year = end_year)
 
+write_results(data = compute_annual_trends(data = timeseries, zyp_method = "yuepilon", start_year = start_year,end_year = end_year), 
+              file = paste0(folder, trendingfolder, "annual_trends_results.xlsx"))
 
-### Annual Trending
-##########################
-trendingfolder <- "8-Trending/"
-dir.create(path=paste0(folder,trendingfolder))
-
-trends <- fasstr_annual_trends_analysis(flowdata=timeseries,
-                               #HYDAT = stn_number,
-                               write_trends_data = T,write_trends_results = T,report_dir = paste0(folder,trendingfolder),
-                               zyp_method = "yuepilon",start_year = start_year,end_year = end_year)
-trends_plots <- fasstr_annual_trends_plots(flowdata=timeseries,
-                                           #HYDAT = stn_number,
-                                           write_plots = T, report_dir = paste0(folder,trendingfolder),
-                                           zyp_method = "yuepilon",start_year = start_year,end_year = end_year)
+write_plots(plots = plot_annual_trends(trends_results = compute_annual_trends(data = timeseries, zyp_method = "yuepilon", start_year = start_year,end_year = end_year),
+                                       zyp_alpha = 0.05),
+            foldername = paste0(folder, trendingfolder , "Annual_Trends"), 
+            type = "pdf",
+            width = 8.5,
+            height = 4,
+            combined_pdf = TRUE)
 
 
 
