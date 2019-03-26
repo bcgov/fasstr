471706f8f5abee70d51f36a0143119133d38bc94
Jon Goetz <jon.goetz@gov.bc.ca>
2019-03-26 09:37:26 -0700

    renamed all tests

--- tests/testthat/test-add_cumulative_volume.R
+++ tests/testthat/test-add_cumulative_volume.R
@@ -1,9 +1,5 @@
 context("Add cumulative volume")
 
-test_that("`add_cumulative_volume()` works", {
-  
-})
-
 test_that("add_cumulative_volume actually adds a column called Cumul_Volume_m3",{
   skip_on_cran()
   skip_on_travis()
@@ -26,10 +22,8 @@ test_that("record starts in the month specified by water_year_start",{
   skip_on_travis()
   month_to_start_water_year <- 5
   stns <- "08NM003"
-  
   water_year_true <- add_cumulative_volume(station_number = stns,  
                                            water_year_start = month_to_start_water_year)
-  
   ## Extract Date of first non na value in Cumul_Volume_m3 column
   Date <- water_year_true[which.min(is.na(water_year_true$Cumul_Volume_m3)),]$Date
   expect_identical(as.numeric(substr(Date, 6, 7)), month_to_start_water_year)
