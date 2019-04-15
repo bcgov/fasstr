7858ac68670e588247b91e045115edef5a8c2df5
Jon Goetz <jon.goetz@gov.bc.ca>
2019-04-15 15:43:56 -0700

    added some tests

--- tests/testthat/test-calc_annual_stats.R
+++ tests/testthat/test-calc_annual_stats.R
@@ -1,5 +1,114 @@
 context("Calc annual stats")
 
-test_that("`calc_annual_stats()` works", {
-  
+test_that("creates a dataframe with the proper columns", {
+  data <- calc_annual_stats(station_number = "08NM116")
+  expect_true(is.data.frame(data) &
+                all(c("Year","Mean","Median","Maximum","Minimum","P10","P90") %in% colnames(data)))
 })
+
+test_that("outputs data for two stations", {
+  data <- calc_annual_stats(station_number = c("08NM116","08HB048"))
+  expect_true(length(unique(data$STATION_NUMBER)) == 2)
+})
+
+test_that("creates a dataframe with custom columns", {
+  data <- calc_annual_stats(station_number = "08NM116",
+                            percentiles = c(25,75))
+  expect_true(all(c("P25","P75") %in% colnames(data)))
+})
+
+test_that("produces NA if there is missing data and warning is produced", {
+  data <- calc_annual_stats(station_number = "08NM116",
+                            ignore_missing = FALSE)
+  expect_true(any(is.na(data)))
+  expect_warning(calc_annual_stats(station_number = "08NM116",
+                                   ignore_missing = FALSE))
+})
+
+test_that("ignore_missing calculates all data", {
+  data <- calc_annual_stats(station_number = "08NM116",
+                            ignore_missing = TRUE)
+  expect_true(any(!is.na(data)))
+})
+
+test_that("data is filtered by years properly", {
+  data <- calc_annual_stats(station_number = "08NM116",
+                            start_year = 1981,
+                            end_year = 2010,
+                            exclude_years = c(1991,1993:1995))
+  expect_identical(1981, min(data$Year))
+  expect_identical(2010, max(data$Year))
+  
+  data_exclude <- dplyr::filter(data, Year %in% c(1991,1993:1995))
+  expect_true(all(is.na(data_exclude[,3:8])))  
+})
+
+test_that("data is summarized by water years properly", {
+  flow_data <- add_date_variables(station_number = "08NM116",
+                                  water_year_start = 10)
+  
+  test_data <- dplyr::filter(flow_data, WaterYear == 1981)
+  test_data <- dplyr::summarise(test_data,
+                                Mean = mean(Value),
+                                Median = median(Value),
+                                Maximum = max(Value),
+                                Minimum = min(Value))
+  
+  data <- calc_annual_stats(data = flow_data,
+                            start_year = 1981,
+                            water_year_start = 10)
+  data <- dplyr::filter(data, Year == 1981)
+  data <- dplyr::select(data, Mean, Median, Maximum, Minimum)
+  
+  expect_equal(test_data, data)
+})
+
+test_that("rolling days are applied properly", {
+  flow_data <- add_date_variables(station_number = "08NM116")
+  flow_data <- add_rolling_means(flow_data,
+                                 roll_days = 7)
+  
+  test_data <- dplyr::filter(flow_data, WaterYear == 1981)
+  test_data <- dplyr::summarise(test_data,
+                                Mean = mean(Q7Day),
+                                Median = median(Q7Day),
+                                Maximum = max(Q7Day),
+                                Minimum = min(Q7Day))
+  
+  data <- calc_annual_stats(data = flow_data,
+                            start_year = 1981,
+                            roll_days = 7)
+  data <- dplyr::filter(data, Year == 1981)
+  data <- dplyr::select(data, Mean, Median, Maximum, Minimum)
+  
+  expect_equal(test_data, data)
+})
+
+test_that("data is filtered by months properly", {
+  flow_data <- add_date_variables(station_number = "08NM116")
+
+  
+  test_data <- dplyr::filter(flow_data, 
+                             WaterYear == 1981,
+                             Month %in% 7:9)
+  test_data <- dplyr::summarise(test_data,
+                                Mean = mean(Value),
+                                Median = median(Value),
+                                Maximum = max(Value),
+                                Minimum = min(Value))
+  
+  data <- calc_annual_stats(data = flow_data,
+                            start_year = 1981,
+                            months = 7:9)
+  data <- dplyr::filter(data, Year == 1981)
+  data <- dplyr::select(data, Mean, Median, Maximum, Minimum)
+  
+  expect_equal(test_data, data)
+})
+
+test_that("transpose properly transposed the results", {
+  data <- calc_annual_stats(station_number = "08NM116",
+                            transpose = TRUE)
+  expect_true(all(c("Mean","Median","Maximum","Minimum","P10","P90") %in% data$Statistic))
+})
+
